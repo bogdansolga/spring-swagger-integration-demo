dd38fea189d790b6b47863d7127c30f745990981
Bogdan Solga <bogdan.solga@gmail.com>
2017-06-28 18:46:16 +0300

    [clean] Cleaned the unused methods

--- src/main/java/com/sg/swagger/demo/repository/ProductRepository.java
+++ src/main/java/com/sg/swagger/demo/repository/ProductRepository.java
@@ -4,18 +4,11 @@ import com.sg.swagger.demo.model.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 /**
  * A simple Spring Data {@link CrudRepository} for the {@link Product} entity
  *
  * @author bogdan.solga
  */
 @Repository
-@SuppressWarnings("unused")
 public interface ProductRepository extends CrudRepository<Product, Integer> {
-
-    List<Product> findByName(final String name);
-
-    List<Product> findByPrice(final double price);
 }
